0ec62cd7af27368573ec2166bb6ef93c22c8121b
eyobN <eyobnigusework@gmail.com>
2025-01-23 04:22:51 +0300

    updated view on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,50 +1,41 @@
-import { FaFacebook,FaInstagram,FaGithub  } from "react-icons/fa";
- 
-import { RiTwitterXFill } from "react-icons/ri";
-import { FaYoutube } from "react-icons/fa6";
+import { FaFacebook, FaInstagram, FaGithub, FaYoutube } from 'react-icons/fa';
+import { RiTwitterXFill } from 'react-icons/ri';
 
-export default function Footer(){
-    return <footer className="bg-white    flex  flex-col">
-    <div className=" mx-auto py-6 flex flex-col  md:justify-between md:items-center md:p-10">
-        <div className="md:flex block  md:space-x-4 space-x-2 md:p-10 text-[15px] md:text-[23px]">
-            <a href="#" className="text-gray-600 hover:text-blue-500">About</a>
-            <a href="#" className="text-gray-600 hover:text-blue-500">Blog</a>
-            <a href="#" className="text-gray-600 hover:text-blue-500">Jobs</a>
-            <a href="#" className="text-gray-600 hover:text-blue-500">Press</a>
-            <a href="#" className="text-gray-600 hover:text-blue-500">Accessibility</a>
-            <a href="#" className="text-gray-600 hover:text-blue-500">Partners</a>
-        </div>
-        <div className="flex space-x-8 mt-4 md:mt-0 text-[30px] p-10">
-            <a href="#">
+const Footer = () => {
+    return (
+        <footer className="bg-white flex flex-col">
+            <div className="flex flex-col justify-around md:justify-between md:items-center md:p-10">
+                <div className="flex flex-wrap justify-between space-x-4 p-8  space-y-6 md:space-y-0 md:p-10 text-[20px] md:text-[23px]">
+                    <a href="#" className="text-gray-600 hover:text-blue-500 md:mr-4">About</a>
+                    <a href="#" className="text-gray-600 hover:text-blue-500 md:mr-4">Blog</a>
+                    <a href="#" className="text-gray-600 hover:text-blue-500 md:mr-4">Jobs</a>
+                    <a href="#" className="text-gray-600 hover:text-blue-500 md:mr-4">Press</a>
+                    <a href="#" className="text-gray-600 hover:text-blue-500 md:mr-4">Accessibility</a>
+                    <a href="#" className="text-gray-600 hover:text-blue-500 md:mr-4">Partners</a>
+                </div>
+                <div className="flex space-x-8 mt-4 md:mt-0 text-[30px] p-10">
+                    <a href="#">
+                        <FaFacebook />
+                    </a>
+                    <a href="#">
+                        <FaInstagram />
+                    </a>
+                    <a href="#">
+                        <RiTwitterXFill />
+                    </a>
+                    <a href="#">
+                        <FaGithub />
+                    </a>
+                    <a href="#">
+                        <FaYoutube />
+                    </a>
+                </div>
+                <div className="text-center text-gray-500 text-sm p-10 text-[22px]">
+                    © 2024 Your Company, Inc. All rights reserved.
+                </div>
+            </div>
+        </footer>
+    );
+};
 
-            <FaFacebook />
-
-            </a>
-            <a href="#">
-
-            <FaInstagram />
-
-            </a>
-            <a href="#">
-
-            <RiTwitterXFill />
-
-            </a>
-            <a href="#">
-            <FaGithub />
-
-            </a>
-            <a href="#">
-                
-            <FaYoutube />
-
-                
-                </a>
-        </div>
-        <div className="text-center text-gray-500 text-sm p-10 text-[22px]">
-        © 2024 Your Company, Inc. All rights reserved.
-    </div>
-    </div>
-  
-</footer>;
-}
+export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,37 @@
+import React, { useState } from "react";
 import { MdOutlineMenu } from "react-icons/md";
-import { FaTwitter } from "react-icons/fa6";
-import { FaTelegram } from "react-icons/fa6";
+import { FaTwitter, FaTelegram } from "react-icons/fa";
 import { CiDesktop } from "react-icons/ci";
+import { IoMdCloseCircle } from "react-icons/io";
 
-export default function Header() {
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="block h-[15vh]">
       <header className="w-full flex justify-between items-baseline h-[45px] pt-[10px] pr-[16px] pb-[10px] pl-[16px]">
-        <div className="md:hidden cursor-pointer">
+        <div className="md:hidden cursor-pointer" onClick={toggleMenu}>
           <MdOutlineMenu size="34px" />
         </div>
-        <div className="md:w-[368px] w-[fit-content]  md:mx-0 mx-auto">
-          <img src="/Link.png" className="h-[32px]" alt="" />
+        <div className="md:w-[368px] w-[fit-content] md:mx-0 mx-auto">
+          <img src="/Link.png" className="h-[32px]" alt="Logo" />
         </div>
 
-        <div className="hidden md:flex relative items-center justify-between w-[350px] h-[40px]  rounded-[9999px] shadow-sm  bg-[#f2f2f2]  pr-[16px] pl-[16px] border-2 border-[rgba(229, 229, 229, 1)] text-[rgba(0, 0, 0, 0.05)]  ">
-          <img src="/search.svg" alt="" />
+        <div className="hidden md:flex relative items-center justify-between w-[350px] h-[40px] rounded-[9999px] shadow-sm bg-[#f2f2f2] pr-[16px] pl-[16px] border-2 border-[rgba(229, 229, 229, 1)] text-[rgba(0, 0, 0, 0.05)]">
+          <img src="/search.svg" alt="Search" />
           <input
             type="text"
             placeholder="Search Agents"
             className="border-none bg-transparent focus:border-none"
           />
-          <img src="/Keyboard.svg" alt="" />
+          <img src="/Keyboard.svg" alt="Keyboard" />
         </div>
         <div className="hidden md:block w-[281px] h-[64px]">
-          <div className="w-[fit-content] h-[64px] left-[127px] p-2 pl-1 gap-0 flex items-center space-x-2 flex-row ">
+          <div className="w-[fit-content] h-[64px] p-2 pl-1 gap-0 flex items-center space-x-2 flex-row">
             <a href="">
               <FaTwitter size="24" />
             </a>
@@ -34,13 +41,48 @@ export default function Header() {
             <a href="">
               <CiDesktop size="36" />
             </a>
-
             <button className="bg-black text-white h-[48px] pr-[24px] pl-[24px] rounded-[4px]">
               Start Buying
             </button>
           </div>
         </div>
       </header>
+
+      {isMenuOpen && (
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 z-50 md:hidden">
+          <div className="absolute top-0 right-0 w-[fit-content] bg-white h-full shadow-lg p-4">
+            <button onClick={toggleMenu} className="text-xl">
+              {" "}
+              <IoMdCloseCircle size="40"/>
+            </button>
+            <div className="w-[fit-content] h-[64px] p-2 pl-1 gap-0 flex items-center space-x-2 flex-row">
+            <a href="">
+              <FaTwitter size="24" />
+            </a>
+            <a href="">
+              <FaTelegram size="24" />
+            </a>
+            <a href="">
+              <CiDesktop size="36" />
+            </a>
+            <button className="bg-black text-white h-[48px] pr-[24px] pl-[24px] rounded-[4px]">
+              Start Buying
+            </button>
+          </div>
+            <div className=" flex relative items-center justify-between w-[280px] h-[40px] rounded-[9999px] shadow-sm bg-[#f2f2f2] pr-[16px] pl-[16px] border-2 border-[rgba(229, 229, 229, 1)] text-[rgba(0, 0, 0, 0.05)]">
+              <img src="/search.svg" alt="Search" />
+              <input
+                type="text"
+                placeholder="Search Agents"
+                className="border-none bg-transparent focus:border-none"
+              />
+              <img src="/Keyboard.svg" alt="Keyboard" />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default Header;
